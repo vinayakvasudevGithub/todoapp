f743f94a41e74397569c68c1d9c8c4bfc74f526b
vinayak <vinayakvasudev9876@gmail.com>
2023-10-15 22:07:30 -0700

    new commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Child from './Child'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Home component</h1>
+        <Child></Child>
+    </div>
+  )
+}
+
+export default Home
