9969da4c9696e5b6810fe19a78c462cabca95bc4
vinayak <vinayakvasudev9876@gmail.com>
2023-10-16 21:08:20 -0700

    16day

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Child = () => {
-    const headingcolor={color:'red'}
+    const headingcolor={color:'blue'}
   return (
     <div>
         <h2 style={headingcolor}>child component of home</h2>
