12db943c20a7ee4203855b1a7cf4ac3928df5b11
vinayak <vinayakvasudev9876@gmail.com>
2023-10-20 20:41:36 -0700

    20/10

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Button,Table,Card,Accordion,Navbar,Nav,Container} from 'react-bootstrap';
 
-const Header = () => {
-
-  let colour='blue'
-
-  const change=()=>{
-    colour='black'
-    console.log(colour)
-  }
+const Header = ({states}) => {
 
   return (
-    <div>
-      <h1>my fav color is {colour}</h1>
-      <Button onClick={change}>change color</Button>
+    <div >
+      {states.map((state)=>(
+        <div className='bg-warning'>
+        <h1>{state.name}</h1>
+        <h4>lang : {state.language}</h4>
+        <h4>population : {state.population}</h4>
+        </div>
+      ))}     
     </div>
   )
 }
