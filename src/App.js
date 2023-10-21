26802aa1f8799e23ab8a5d64731eb2733ff5a5b6
vinayak <vinayakvasudev9876@gmail.com>
2023-10-20 23:44:01 -0700

    20/10commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import { Container } from 'react-bootstrap';
 import './App.css';
-import Header from "./components/Header"
-import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 function App() {
 
-  const states=[
-    {name: 'kerala',language: 'malayalam',population:1000},
-    {name: 'thamilnadu',language: 'tamil',population:2000},
-    {name: 'karnataka',language: 'kannada',population:3000}
-]
-
   return (
     <div className='App'>
-      <Container>
-      <Header states={states} />
-      </Container>
+      <Header></Header>
     </div>
   );
 }
