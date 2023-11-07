a4dd4868ad19c0ebd0db6712b990a0eada4b3061
vinayak <vinayakvasudev9876@gmail.com>
2023-11-06 16:44:40 -0800

    06/10

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {Button,Table,Card,Accordion,Navbar,Nav,Container} from 'react-bootstrap';
 
 const Header = () => {
-    const [counter,setCounter]=useState(0)
-  return (
 
+  const [posts,setPosts]=useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => setPosts(posts.splice(0,10)))
+  })
+
+  return (
     <div>
-      <h1>{counter}</h1>
-      <button onClick={()=>setCounter(counter+12)}>count+</button>
+      <h1>fetch Appi data</h1>
+      {posts.map((post)=>
+      <div key={post.id}>
+        <h1>{post.title}</h1> 
+        <p>{post.body}</p>  
+        </div>     
+      )}
     </div>
   )
 }
