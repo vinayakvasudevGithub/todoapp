ff8286c10e5fd0539b193bfafb8f09d4581c0f38
vinayak <vinayakvasudev9876@gmail.com>
2023-11-07 21:46:58 -0800

    07/10

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,9 @@ import {Button,Table,Card,Accordion,Navbar,Nav,Container} from 'react-bootstrap'
 
 const Header = () => {
 
-  const [posts,setPosts]=useState([])
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(posts => setPosts(posts.splice(0,10)))
-  })
-
   return (
     <div>
-      <h1>fetch Appi data</h1>
-      {posts.map((post)=>
-      <div key={post.id}>
-        {/* 06/10 */}
-        <h1>{post.title}</h1> 
-        <p>{post.body}</p>  
-        </div>     
-      )}
+        <h1>hii</h1>
     </div>
   )
 }
